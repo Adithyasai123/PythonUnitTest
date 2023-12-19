55c5df9a0b838f9f9d775ff7d9d89cd25bfd67d0
Adithya sai <123888728+Adithyasai123@users.noreply.github.com>
2023-12-19 12:20:05 +0530

    Update test_website_connection.py

--- test_website_connection.py
+++ test_website_connection.py
@@ -13,9 +13,11 @@ class TestWebsiteConnection(unittest.TestCase):
             # Check if the response status code indicates a successful connection
             self.assertEqual(response.status_code, 200, f"Failed to load website. Status code: {response.status_code}")
 
-            print("Website loaded successfully!")
+            # Print a message indicating a successful connection
+            print("Connected to the website. Connection is up!")
 
         except requests.RequestException as e:
+            # Print an error message if the connection fails
             print(f"Failed to connect to the website: {e}")
             self.fail("Website failed to load.")
 
